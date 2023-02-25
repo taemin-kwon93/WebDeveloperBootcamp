48869433900e26537eff80d378559e5f047fe1aa
taemin-kwon93 <4146kt@gmail.com>
2023-02-25 17:03:26 +0900

    study: make random number

---
+++ learn-javascript/learn_math.js
@@ -0,0 +1,8 @@
+const makeRandomNumber = () => {
+  const randomNumber = (Math.random())*10;
+  console.log('randomNumber: ', randomNumber);
+  let oneToTen = Math.floor(randomNumber);
+  console.log('oneToTen: ', oneToTen);
+}
+
+makeRandomNumber();

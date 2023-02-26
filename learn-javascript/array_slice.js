7baec4fef89e7c7fefff56481ac2ed0655f02a02
taemin-kwon93 <4146kt@gmail.com>
2023-02-26 22:22:59 +0900

    study: slice

---
+++ learn-javascript/array_slice.js
@@ -0,0 +1,4 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet'];
+colors.slice(0);
+// start and end represent the index of items in that array
+console.log(colors.slice(1));

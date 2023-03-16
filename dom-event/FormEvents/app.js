d9aa98654c7fbbec10de9d98fa4b0bac0ebc92d8
taemin-kwon93 <4146kt@gmail.com>
2023-03-17 07:05:19 +0900

    study

--- dom-event/FormEvents/app.js
+++ dom-event/FormEvents/app.js
@@ -1,7 +1,16 @@
 const tweetForm = document.querySelector('#tweetForm')
+const tweetsContainer = document.querySelector('#tweets');
 tweetForm.addEventListener('submit', function(e) {
-    const usernameInput = document.querySelector('input')[0];
-    const tweetInput = document.querySelector('input')[1];
+    e.preventDefault()
+    // const usernameInput = document.querySelector('input')[0];
+    // const tweetInput = document.querySelector('input')[1];
+    const username = tweetForm.elements.username;
+    const tweet = tweetForm.elements.tweet;
+
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    username.value = '';
+    tweet.value = '';
 });
 
 

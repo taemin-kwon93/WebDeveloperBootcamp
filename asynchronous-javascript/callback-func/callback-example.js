9295e553d449e384f332d5ba2971f1063dad2c31
taemin-kwon93 <4146kt@gmail.com>
2023-03-23 08:18:03 +0900

    study

--- asynchronous-javascript/callback-func/callback-example.js
+++ asynchronous-javascript/callback-func/callback-example.js
@@ -15,3 +15,33 @@ fakeRequestCallback('http://test.com', function() {
 }, function() {
   console.log('failure')
 })
+
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * (4500)) + 500;
+    console.log(delay)
+    setTimeout(() => {
+      console.log('setTimeout start')
+      if(delay > 4000) {
+        reject('Connection Timeout')
+      } else {
+        resolve(`Here is your fake data from ${url}`)
+      }
+    }, delay)
+    console.log('after setTimeout')
+  })
+}
+
+fakeRequestPromise('test url1')
+  .then(() => {
+    console.log('test url 1 _ Promise1')
+    fakeRequestPromise('test url2')
+      .then(() => {
+        console.log('test url 2 _ Promise2')
+      }).catch(() => {
+      console.log("Promise rejected2")
+    })
+  })
+  .catch(() => {
+    console.log("Promise rejected1")
+  })

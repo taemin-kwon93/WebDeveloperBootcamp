8f521de77ccd72ebd0464b24efcc5f9d13b469b8
taemin-kwon93 <4146kt@gmail.com>
2023-03-23 22:05:13 +0900

    study

--- asynchronous-javascript/callback-func/callback-example.js
+++ asynchronous-javascript/callback-func/callback-example.js
@@ -1,20 +1,20 @@
-const fakeRequestCallback = (url, success, failure) => {
-  const delay = Math.floor(Math.random() * 4500) + 500;
-  console.log('delay: ',delay)
-  setTimeout(() => {
-    if(delay > 4000) {
-      failure('Connection Timeout')
-    } else {
-      success(`Here is your fake data from ${url}`)
-    }
-  }, delay)
-}
-
-fakeRequestCallback('http://test.com', function() {
-  console.log('success')
-}, function() {
-  console.log('failure')
-})
+// const fakeRequestCallback = (url, success, failure) => {
+//   const delay = Math.floor(Math.random() * 4500) + 500;
+//   console.log('delay: ',delay)
+//   setTimeout(() => {
+//     if(delay > 4000) {
+//       failure('Connection Timeout')
+//     } else {
+//       success(`Here is your fake data from ${url}`)
+//     }
+//   }, delay)
+// }
+//
+// fakeRequestCallback('http://test.com', function() {
+//   console.log('success')
+// }, function() {
+//   console.log('failure')
+// })
 
 const fakeRequestPromise = (url) => {
   return new Promise((resolve, reject) => {
@@ -32,16 +32,37 @@ const fakeRequestPromise = (url) => {
   })
 }
 
+// fakeRequestPromise('test url1')
+//   .then(() => {
+//     console.log('test url 1 _ Promise1')
+//     fakeRequestPromise('test url2')
+//       .then(() => {
+//         console.log('test url 2 _ Promise2')
+//       }).catch(() => {
+//       console.log("Promise rejected2")
+//     })
+//   })
+//   .catch(() => {
+//     console.log("Promise rejected1")
+//   })
+
+
 fakeRequestPromise('test url1')
-  .then(() => {
-    console.log('test url 1 _ Promise1')
-    fakeRequestPromise('test url2')
-      .then(() => {
-        console.log('test url 2 _ Promise2')
-      }).catch(() => {
-      console.log("Promise rejected2")
-    })
+  .then((data) => {
+    console.log('url1 success')
+    console.log('data1:', data)
+    return fakeRequestPromise('test url2')
+  })
+  .then((data) => {
+    console.log('url2 success')
+    console.log('data2:', data)
+    return fakeRequestPromise('test url3')
+  })
+  .then((data) => {
+    console.log('url3 success')
+    console.log('data3:', data)
+    return fakeRequestPromise('test url4')
   })
   .catch(() => {
-    console.log("Promise rejected1")
+    console.log('catch error')
   })
---
+++ asynchronous-javascript/custom-promise/app.js
@@ -0,0 +1,31 @@
+const PersonA = {
+  age: '23',
+  name: 'john'
+}
+const PersonB = {
+  age: '33',
+  name: 'kim'
+}
+const People = [ PersonA, PersonB ]
+
+
+const fakeRequest = (url) => {
+  console.log('fakeRequest')
+  return new Promise((resolve, reject) => {
+    const randomNumber = Math.random();
+    setTimeout(() => {
+      console.log(`${url} set time out`)
+        randomNumber < 0.7? resolve(People) : reject('rejected');
+    }, 3000)
+ })
+}
+
+fakeRequest('/dogs/1')
+  .then((data) => {
+    console.log('resolved')
+    console.log('data:', data)
+  })
+  .catch((err) => {
+    console.log('rejected')
+    console.log('rejected err message:', err)
+  })

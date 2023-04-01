688094447b8cc042fd1bd23de1cebbf594a762c9
taemin-kwon93 <4146kt@gmail.com>
2023-04-01 18:32:51 +0900

    study

---
+++ learn-module/index.js
@@ -0,0 +1,6 @@
+const blue = require('./blue')
+const janet = require('./janet')
+const sadie = require('./sadie')
+
+const allCats = [blue, janet, sadie];
+console.log(allCats)
---
+++ learn-module/math.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  return a + b;
+}
+
+const minus = (a, b) => a - b;
+
+const PI = 3.14159
+
+const square = x => x * x;
+
+// module.exports = "HELLO"
+module.exports = {
+  add, minus, PI, square
+};
